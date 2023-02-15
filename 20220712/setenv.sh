5fea76aed17af3067bf37ee059de432f47e5d97a
RBenassiUnimore <241673@studenti.unimore.it>
2023-02-15 11:44:04 +0100

    20220712 pubsub

--- 20220712/setenv.sh
+++ 20220712/setenv.sh
@@ -3,3 +3,11 @@
 export PROJECT_ID=rcbns-20220712
 
 export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/credentials.json"
+
+export TOPIC=bracelet_req
+
+export PUSH_SUBSC_NAME=bracelet_req_push_sub
+
+export PULL_SUBSC_NAME=bracelet_req_pull_sub
+
+export TOKEN=zxcvtoken
